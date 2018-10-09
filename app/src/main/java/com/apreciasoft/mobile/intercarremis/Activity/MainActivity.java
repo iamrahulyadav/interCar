b5120bbb583f5d6442d3e847d26caceb56b6311e
Admin <Admin>
2018-10-09 13:43:47 -0400

    - add Taximetro - Add Fabric - Add Firebase new Account - Clean code

--- app/src/main/java/com/apreciasoft/mobile/intercarremis/Activity/MainActivity.java
+++ app/src/main/java/com/apreciasoft/mobile/intercarremis/Activity/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "NOTICIAS";
     public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
     protected PowerManager.WakeLock wakelock;
-    public static String version = "2.0.14";
+    public static String version = "2.0.16";
     public ProgressDialog loading;
     ServicesLoguin apiService = null;
     public GlovalVar gloval = null;
